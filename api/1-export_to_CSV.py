cfb4bc17ef49968f3988ff499effcc8b3c2b8a54
uoch <chaouch.thameurtc61@gmail.com>
2023-04-29 08:39:42 +0100

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Using what you did in the task #0, extend your Python script to export data in the CSV format.
+Using what you did in the task #0, 
+extend your Python script 
+to export data in the CSV format.
 """
 import requests
 import sys
