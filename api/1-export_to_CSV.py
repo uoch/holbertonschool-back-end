4fae0dc64126183fb00eb64594dba25c3359a99a
uoch <chaouch.thameurtc61@gmail.com>
2023-04-29 08:37:53 +0100

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 """Using what you did in the task #0, extend your Python script to export data in the CSV format.
 """
-
 import requests
 import sys
 import csv
+import importlib
+
+module = importlib.import_module('0-gather_data_from_an_API')
+getdata = module.getdata
 
 # Retrieve employee data
-employee_id = sys.argv[1]
-employee_response = requests.get('https://jsonplaceholder.typicode.com/users/' + employee_id)
-employee_data = employee_response.json()
-
-todos_response = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
-todos_data = todos_response.json()
-
-employee_name = employee_data['username']
+employee_id = int(sys.argv[1])
+employe_name, tasks_completed, total, todos_data = getdata(employee_id)
+print(todos_data)
 
 # Write data to CSV file
-with open(f'{employee_id}.csv', 'w') as csv_file:
-    csv_writer = csv.writer(csv_file, delimiter=',')
+with open(f'{employee_id}.csv', 'w') as f:
+    csv_writer = csv.writer(f,quoting=csv.QUOTE_MINIMAL, quotechar='"',delimiter=',')
 
     # Write header row
     csv_writer.writerow(['id', 'name', 'completed', 'title'])
+    for todo in todos_data:
+        csv_writer.writerow([str(employee_id), employe_name, str(todo['completed']), todo['title']])
 
+
+"""
     # Write data rows
     for todo in todos_data:
-        completed_status = todo['completed']
-        task_title = todo['title']
-        csv_writer.writerow([employee_id, employee_name, completed_status, task_title])
-
+        tasks_completed = todos_data['completed']
+        task_title = todos_data['title']
+        csv_writer.writerow([employee_id, employee_name, completed_status, task_title])"""
