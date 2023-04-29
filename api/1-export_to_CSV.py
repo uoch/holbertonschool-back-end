09566e73a770af35d5b0319d1081153073aa36b5
uoch <chaouch.thameurtc61@gmail.com>
2023-04-29 08:55:44 +0100

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 """
-Using what you did in the task #0,
-extend your Python script
-to export data in the CSV format.
+Using what you did in the task #0, extend your
+Python script to export data in the CSV format.
 """
-import requests
-import sys
-import csv
-import importlib
-"""
-Using what you did in the task #0,
-extend your Python script
-to export data in the CSV format.
-"""
-module = importlib.import_module('0-gather_data_from_an_API')
-getdata = module.getdata
 
-employee_id = int(sys.argv[1])
-employe_name, tasks_completed, total, todos_data = getdata(employee_id)
 
-with open(f'{employee_id}.csv', 'w') as f:
-    csv_writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL,
-                            quotechar='"', delimiter=',')
+if __name__ == '__main__':
+    import csv
+    import requests
+    import sys
 
-    # Write header row
-    csv_writer.writerow(['id', 'name', 'completed', 'title'])
-    for todo in todos_data:
-        csv_writer.writerow([str(employee_id), employe_name,
-                            str(todo['completed']), todo['title']])
+    t = sys.argv[1]
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + t)
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' + t)
+
+    employee = response.json()
+    todos = todos.json()
+    id = employee['id']
+    name = employee['username']
+
+    with open(f'{t}.csv', 'w') as file:
+        for i in todos:
+            Ta = i['completed']
+            TASK_TITLE = i['title']
+            TASK_COMPLETED_STATUS = i['completed']
+            TASK_TITLE = i['title']
+            file.write(
+                f"\"{id}\",\"{name}\",\"{Ta}\",\"{TASK_TITLE}\"\n")
