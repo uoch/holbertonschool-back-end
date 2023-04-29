983fe4493c71b4a9b226f5607bc7e6e77c8ba736
uoch <chaouch.thameurtc61@gmail.com>
2023-04-29 07:32:08 +0100

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,27 +8,25 @@ import requests
 import sys
 
 
-def get_todo_list_data(employee_id):
-    employee_data = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{employee_id}").json()
-    employee_name = employee_data["name"]
+def getdata(employee_id):
+    data = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)).json()
+    employee_name = data["name"]
 
     todos_data = requests.get(
         f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}").json()
 
-    total_number_of_tasks = len(todos_data)
-    number_of_done_tasks = len(
-        [task for task in todos_data if task["completed"]])
+    total = len(todos_data)
+    tasks_completed = len([task for task in todos_data if task["completed"]])
 
-    return employee_name, number_of_done_tasks, total_number_of_tasks, todos_data
+    return employee_name, tasks_completed, total, todos_data
 
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
-    employee_name, number_of_done_tasks, total_number_of_tasks, todos_data = get_todo_list_data(
-        employee_id)
+    employee_name, tasks_completed, total, todos_data = getdata(employee_id)
 
-    progress = f"{number_of_done_tasks}/{total_number_of_tasks}"
+    progress = "{:.2%}".format(tasks_completed/total)
 
     print("Employee {} is done with tasks({}):".format(employee_name, progress))
     for task in todos_data:
