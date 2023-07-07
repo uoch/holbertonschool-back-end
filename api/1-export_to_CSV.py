89fe4c73517d274f6ac7ef83a40c7af3f41d24e1
uoch <chaouch.thameurtc61@gmail.com>
2023-07-07 17:22:36 +0100

    Your commit message

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """
-Using what you did in the task #0,
-extend your Python script
-to export data in the CSV format.
+Using what you did in the task #0, extend your
+Python script to export data in the CSV format.
 """
+
+
 if __name__ == '__main__':
     import csv
     import requests
     import sys
 
-    num = sys.argv[1]
+    t = sys.argv[1]
     response = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' + num)
+        'https://jsonplaceholder.typicode.com/users/' + t)
     todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId=' + num)
+        'https://jsonplaceholder.typicode.com/todos?userId=' + t)
 
     employee = response.json()
     todos = todos.json()
     id = employee['id']
     name = employee['username']
 
-    with open(f'{num}.csv', 'w') as file:
+    with open(f'{t}.csv', 'w') as file:
         for i in todos:
             Ta = i['completed']
             TASK_TITLE = i['title']
