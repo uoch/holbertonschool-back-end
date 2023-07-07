5e35be008c4198c8c36c6570c39e6e8e76d59e6b
uoch <chaouch.thameurtc61@gmail.com>
2023-07-07 17:05:34 +0100

    Your commit message

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,15 +6,26 @@ list progress.
 """
 import requests
 import sys
-from funct import getdata
-
 if __name__ == "__main__":
+    # Retrieve employee data
     employee_id = int(sys.argv[1])
-    employe_name, tasks_completed, total, todos_data = getdata(employee_id)
+    employee_data = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}").json()
+    EMPLOYEE_NAME = employee_data["name"]
 
-    progress = "{}/{}".format(tasks_completed, total)
+    # Retrieve todo list for employee
+    adress = "https://jsonplaceholder.typicode.com/todos?userId="
+    todos_data = requests.get(
+        f"{adress}{employee_id}").json()
+    # Calculate progress
+    TOTAL_NUMBER_OF_TASKS = len(todos_data)
+    NUMBER_OF_DONE_TASKS = len(
+        [task for task in todos_data if task["completed"] is True])
+    progress = f"{NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}"
 
-    print("Employee {} is done with tasks({}):".format(employe_name, progress))
-    for task in todos_data:
-        if task["completed"]:
-            print(f"\t{task['title']}")
+    # Print progress and completed tasks
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks({progress}):")
+    for TASK_TITLE in todos_data:
+        if TASK_TITLE["completed"] is True:
+            print(f"\t {TASK_TITLE['title']}")
+
